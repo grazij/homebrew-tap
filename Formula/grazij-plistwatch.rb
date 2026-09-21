c0a1b30a0d6a37b99cd992a1eab56ee3f74e86dc
Jose Graziani <1012717+grazij@users.noreply.github.com>
2026-09-21 08:59:34 -0400

    grazij-plistwatch 2025.09.24+grazij.5

--- Formula/grazij-plistwatch.rb
+++ Formula/grazij-plistwatch.rb
@@ -1,9 +1,9 @@
 class GrazijPlistwatch < Formula
   desc "Watch macOS defaults and print the commands that recreate each change"
   homepage "https://github.com/grazij/plistwatch"
-  url "https://github.com/grazij/plistwatch/archive/refs/tags/v2025.09.24%2Bgrazij.4.tar.gz"
-  version "2025.09.24+grazij.4"
-  sha256 "5a3eff06da4934b07d450dc5f2c376591baefc076482e1090a2c2b0028269ec0"
+  url "https://github.com/grazij/plistwatch/archive/refs/tags/v2025.09.24%2Bgrazij.5.tar.gz"
+  version "2025.09.24+grazij.5"
+  sha256 "7369d8ff6ccea36912ccf3c2de322f91e8c1373c827ee45f28307d491c64c6f4"
   license "MIT"
   head "https://github.com/grazij/plistwatch.git", branch: "main"
 
